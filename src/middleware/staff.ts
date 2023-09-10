afb481521176287d145ef551d814b802d2d33178
Kodamas <superjason95@gmail.com>
2023-09-10 11:26:19 +0200

    fix: delete staff middleware

--- src/middleware/staff.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  const authUser = useAuthUser()
-
-  if (authUser.value.id === -1) { return navigateTo('/') } else if (authUser.value.rank < 6) { return navigateTo('/home') }
-})
